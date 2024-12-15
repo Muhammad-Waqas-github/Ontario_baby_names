0b1ab5ecea8e9dd796c6565df07050b56e3b8f76
Muhammad Waqas <mwaqasashfaque@gmail.com>
2024-12-14 23:04:21 -0500

    fixed link and changes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,41 @@ import Chart from "chart.js/auto";
 import "chartjs-adapter-date-fns";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Switch from "@mui/material/Switch";
-
+import { Typography } from "@mui/material";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 function App() {
   const [name, setName] = useState("");
   const [parsedData, setParsedData] = useState([]);
   const [chart, setChart] = useState(null);
   const [selectedNames, setSelectedNames] = useState([]);
   const [selectedDataset, setSelectedDataset] = useState("male");
+
+  useEffect(() => {
+    const handleResize = () => {
+      const canvas = document.getElementById("frequencyChart");
+      canvas.width = window.innerWidth * 0.8; // 80% of window width
+      canvas.height = window.innerHeight * 0.3; // 30% of window height
+      if (chart) {
+        chart.resize();
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial resize
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [chart]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -125,7 +152,20 @@ function App() {
     }
     return color;
   };
+  const getEarliestAndLatestYears = (tableData) => {
+    const years = [];
+    Object.keys(tableData).forEach((name) => {
+      tableData[name].forEach((row) => {
+        years.push(parseInt(row["Year/Annee"]));
+      });
+    });
+    return Array.from(new Set(years)).sort((a, b) => a - b);
+  };
 
+  const getFrequencyForYear = (data, year) => {
+    const row = data.find((item) => item["Year/Annee"] === year.toString());
+    return row ? row["Frequency/Frequence"] : "";
+  };
   const toggleDataset = () => {
     setSelectedDataset((prevDataset) =>
       prevDataset === "male" ? "female" : "male"
@@ -136,9 +176,45 @@ function App() {
       setChart(null);
     }
   };
+  const [tableData, setTableData] = useState({});
 
+  const addToTable = (name) => {
+    if (!parsedData || parsedData.length === 0 || !name.trim()) {
+      return;
+    }
+
+    const filteredResults = parsedData.filter(
+      (row) => row["Name/Nom"] && row["Name/Nom"].trim() === name.toUpperCase()
+    );
+
+    if (filteredResults.length > 0) {
+      setTableData((prevData) => ({ ...prevData, [name]: filteredResults }));
+    } else {
+      console.log("Name not found in data");
+    }
+  };
+
+  useEffect(() => {
+    if (selectedNames.length > 0) {
+      selectedNames.forEach((name) => addToTable(name));
+    }
+  }, [selectedNames, parsedData]);
   return (
     <div style={{ margin: "20px" }}>
+      <Typography
+        variant="h3"
+        component="h1"
+        sx={{
+          fontSize: { xs: "1.5rem", md: "2.5rem" },
+          fontWeight: 600,
+          color: "#1976d2", // primary color
+          textTransform: "none",
+          textAlign: "center",
+          mb: 2,
+        }}
+      >
+        Is My Name Popular?
+      </Typography>
       <TextField
         id="standard-basic"
         label="Enter a name"
@@ -150,13 +226,75 @@ function App() {
         Add to Chart
       </Button>
       <div>
-        <Switch checked={selectedDataset === "male"} onChange={toggleDataset} />
-        <span>Show {selectedDataset === "male" ? "Male" : "Female"} Data</span>
+        <RadioGroup value={selectedDataset} onChange={toggleDataset}>
+          <FormControlLabel
+            value="male"
+            control={<Radio />}
+            label="Show Male Data"
+          />
+          <FormControlLabel
+            value="female"
+            control={<Radio />}
+            label="Show Female Data"
+          />
+        </RadioGroup>
       </div>
       <div>
         <p>Selected Names: {selectedNames.join(", ")}</p>
-        <canvas id="frequencyChart" width="400" height="200"></canvas>
+        <canvas id="frequencyChart"></canvas>
       </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ width: "max-content" }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Year</TableCell>
+              {Object.keys(tableData).map((name) => (
+                <TableCell key={name} align="right">
+                  {name}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(tableData).length > 0 &&
+              getEarliestAndLatestYears(tableData).map((year) => (
+                <TableRow
+                  key={year}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {year}
+                  </TableCell>
+                  {Object.keys(tableData).map((name) => (
+                    <TableCell key={name} align="right">
+                      {getFrequencyForYear(tableData[name], year)}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Typography
+        variant="body2"
+        component="p"
+        sx={{
+          fontSize: ".75rem",
+          fontWeight: 400,
+          color: "#666", // neutral color
+          textTransform: "none",
+          textAlign: "center",
+          mt: 4,
+          pb: 2,
+        }}
+      >
+        &copy; {new Date().getFullYear()} Muhammad Waqas. All Rights Reserved.{" "}
+        <br />
+        The data represented herein pertains to baby names registered in Ontario
+        till 2019, excluding names registered five times or less. This data is
+        provided for informational purposes only and does not constitute
+        official records or endorsement.
+      </Typography>
     </div>
   );
 }
